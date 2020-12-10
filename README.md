a695deac80d30cf42385fb94bd4b74d68fba31bf
Soren Madsen <soren.j.madsen@gmail.com>
2020-12-09 19:06:38 -0500

    Backend work
    Update server.js
    Add API Usage to README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Queue-Manager
 Lab TAs of Santa Clara University, struggle no longer with managing students' questions, for the latest and greatest technology is here! With the Fantastical Queue Manager, you can manage every question from every student anywhere and any time.
+
+## Node.js Server Usage
+$ node server.js --port=[NUM]
+    - NUM is user-defined.
+
+## RESTful API Usage:
+GET / 
+localhost:[PORT]
+    General
+                /api                  : Welcome to the API!
+                /statistics           : Returns some simple user statistics
+    Questions
+                /questions/all        : Returns all questions asked in all labs
+                /questions/[LAB_NAME] : Returns all questions asked in specified lab
+    Resources
+                /resources/all        : Returns all resources posted in all labs
+                /resources/[LAB_NAME] : Returns all resources posted in specified lab
+
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const fsSync = require('fs')
 const fs = require('fs/promises')
 const path = require('path')
 
 const dataFile = "./data.json"
 const dataTemplate = {
-    questions : [],
-    resources : [],
-    statistics : {}
+    questions : {},
+    resources : {},
+    statistics : {
+        resourcesPosted: 0,
+        questionsAsked: 0,
+        questionsAnswered: 0,
+        lastQuestionPosted : "N/A",
+        lastResourcePosted : "N/A"
+    }
 }
 
 const app = express()
@@ -25,34 +32,120 @@ if (args.length === 0){
 app.use(express.json())
 app.use(express.static('static'))
 
-// API Endpoints
-app.get('/api')
-app.get('/stats')
-app.get('/questions')
-app.get('/resources')
-app.post('/questions')
-app.post('/resources')
+// Function: getAPI
+// Description: Welcomes the dev to the API!
+const getAPI = (req, res) => {
+    res.status(200).send("You've reached the Queue Manager API!")
+}
 
+// Function: getAllQuestions
+// Description: Returns all the questions asked by all lab classes.
+const getAllQuestions = (req, res) => {
+    if(!res.app.locals.data.questions)
+        res.status(404).send("No questions asked!")    
+    else   
+        res.status(200).send({questions : res.app.locals.data.questions})
+}
 
+// Function: getLabQuestions
+// Description: Returns all questions asked in the specified lab.
+const getLabQuestions = (req, res) => {
+    const lab = req.params.lab
+    if(Object.keys(res.app.locals.data.questions).includes(lab))
+        res.status(200).send({questions : res.app.locals.data.questions[lab]})
+    else
+        res.status(404).send(`No questions for class: ${lab}`)
+}
+
+// Function: getAllResources
+// Description: Returns all the resources posted in all lab classes.
+const getAllResources = (req, res) => {
+    if(!res.app.locals.data.resources)
+        res.status(404).send("No resources posted!")    
+    else   
+        res.status(200).send({resources : res.app.locals.data.resources})
+}
+
+// Function: getLabResources
+// Description: Returns all resources posted in the specified lab.
+const getLabResources = (req, res) => {
+    const lab = req.params.lab
+    if(Object.keys(res.app.locals.data.resources).includes(lab))
+        res.status(200).send({resources : res.app.locals.data.resources[lab]})
+    else
+        res.status(404).send(`No resources posted for class: ${lab}`)
+}
+
+// Function: getSiteStats
+// Description: Gives some basic user statistics about the website
+const getSiteStats = (req, res) => {
+    res.status(200).send(res.app.locals.data.statistics)
+}
+
+// Function: setupServer
+// Description: Handles network functions and restores saved data to memory.
 const setupServer = (data) => {
-    app.locals.data = data;
+    app.locals["data"] = data;
     app.listen(port, () => {
         console.log(`Queue Manager started on http://localhost:${port}`);
     });
 }
 
+// Function: saveData
+// Description: Saves any session data to JSON for recovery/storage.
 const saveData = (data) => {
     fs.writeFile(dataFile, JSON.stringify(data), 'utf-8');
 }
 
-try {
-    if (fs.existsSync(dataFile)) {
-        fs.readFile(dataFile, "utf-8")
-        .then((fileContents) => JSON.parse(fileContents))
-        .then((data) => {
-            setupServer(data)
-        });
+// Function: askQuestion
+// Description: Adds a question to the board of a specified lab. Returns the id of the question.
+const askQuestion = (req, res) => {
+    const lab = req.params.lab
+    if(!Object.keys(res.app.locals.data.questions).includes(lab)){
+        res.app.locals.data.questions[lab] = [req.body]
+        const id = res.app.locals.data.questions[lab].length
+        res.status(200).send({id : id})
     }
-} catch(err) {
-    setupServer(dataTemplate)
+    else {
+        res.app.locals.data.questions[lab].push(req.body)
+        const id = res.app.locals.data.questions[lab].length
+        res.status(200).send({id : id})
+    }
 }
+
+// Function: postResource
+// Description: Posts a resource to the board of a specified lab. Returns the assigned id of the resource.
+const postResource = (req, res) => {
+    const lab = req.params.lab
+    if(!Object.keys(res.app.locals.data.resources).includes(lab)){
+        res.app.locals.data.resources[lab] = [req.body]
+        const id = res.app.locals.data.resources[lab].length
+        res.status(200).send({id : id})
+    }
+    else {
+        res.app.locals.data.resources[lab].push(req.body)
+        const id = res.app.locals.data.resources[lab].length
+        res.status(200).send({id : id})
+    }
+}
+
+// API Endpoints
+app.get('/api', getAPI)
+app.get('/stats', getSiteStats)
+app.get('/questions/all', getAllQuestions)
+app.get('/resources/all', getAllResources)
+app.get('/questions/:lab', getLabQuestions)
+app.get('/resources/:lab', getLabResources)
+app.post('/questions/:lab', askQuestion)
+app.post('/resources/:lab', postResource)
+
+// Check for saved data, then finish server setup
+fs.readFile(dataFile, "utf-8")
+.then((fileContents) => JSON.parse(fileContents))
+.then((data) => {
+    setupServer(data)
+})
+.catch((err) => {
+    setupServer(dataTemplate)
+    saveData(app.locals.data)
+})
