ccccb82e7e1e14f98aa1baa7893a35ecac1ab2eb
Aditya Mohan <adityamohan@Adityas-MacBook-Pro.local>
2020-12-10 19:48:05 -0800

    upload resources new resource list page

--- static/resources.js
+++ static/resources.js
@@ -6,27 +6,27 @@ function listResources() {
                 for (const resource of lab) {
                     const li = document.createElement("li")
                     if(resource.resource.lab==1){
-                        li.textContent = JSON.stringify(resource.resource.files) + JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
+                        li.textContent = JSON.stringify(resource.resource.author) + JSON.stringify(resource.resource.files) + JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
                         document.getElementById("list1").appendChild(li)
                     }
                     if(resource.resource.lab==2){
-                        li.textContent = JSON.stringify(resource.resource.files) + JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
+                        li.textContent = JSON.stringify(resource.resource.author) + JSON.stringify(resource.resource.files) + JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
                         document.getElementById("list2").appendChild(li)
                     }
                     if(resource.resource.lab==3){
-                        li.textContent = JSON.stringify(resource.resource.files)+ JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
+                        li.textContent = JSON.stringify(resource.resource.author) + JSON.stringify(resource.resource.files)+ JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
                         document.getElementById("list3").appendChild(li)
                     }
                     if(resource.resource.lab==4){
-                        li.textContent = JSON.stringify(resource.resource.files)+ JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
+                        li.textContent = JSON.stringify(resource.resource.author) + JSON.stringify(resource.resource.files)+ JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
                         document.getElementById("list4").appendChild(li)
                     }
                     if(resource.resource.lab==5){
-                        li.textContent = JSON.stringify(resource.resource.files)+ JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
+                        li.textContent = JSON.stringify(resource.resource.author) + JSON.stringify(resource.resource.files)+ JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
                         document.getElementById("list5").appendChild(li)
                     }
                     if(resource.resource.lab==6){
-                        li.textContent = JSON.stringify(resource.resource.files)+ JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
+                        li.textContent = JSON.stringify(resource.resource.author) + JSON.stringify(resource.resource.files)+ JSON.stringify(resource.resource.text) + " Date and Time Submitted: " + JSON.stringify(resource.resource.date)
                         document.getElementById("list6").appendChild(li)
                     }
                 }
