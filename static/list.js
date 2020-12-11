fed87e041e435733588a13a9d6f84deca35c916e
Arjun Chimni <arjun.chimni@gmail.com>
2020-12-10 16:03:34 -0800

    Sorted into Lab numbers
    Each question is sorted from Lab #s

--- static/list.js
+++ static/list.js
@@ -2,13 +2,33 @@ function listQuestions() {
     fetch('/questions/all')
         .then(res => res.ok ? res.json() : "No questions!")
         .then(body => {
-            console.log(body)
             for (const lab of Object.values(body.questions)) {
                 for (const question of lab) {
                     const li = document.createElement("li")
-                    li.textContent = JSON.stringify(question.question.text)
-                    document.getElementById("list").appendChild(li)
-                    console.log(question)
+                    if(question.question.lab==1){
+                        li.textContent = JSON.stringify(question.question.text) + " Date and Time Submitted: " + JSON.stringify(question.question.date)
+                        document.getElementById("list1").appendChild(li)
+                    }
+                    if(question.question.lab==2){
+                        li.textContent = JSON.stringify(question.question.text) + " Date and Time Submitted: " + JSON.stringify(question.question.date)
+                        document.getElementById("list2").appendChild(li)
+                    }
+                    if(question.question.lab==3){
+                        li.textContent = JSON.stringify(question.question.text) + " Date and Time Submitted: " + JSON.stringify(question.question.date)
+                        document.getElementById("list3").appendChild(li)
+                    }
+                    if(question.question.lab==4){
+                        li.textContent = JSON.stringify(question.question.text) + " Date and Time Submitted: " + JSON.stringify(question.question.date)
+                        document.getElementById("list4").appendChild(li)
+                    }
+                    if(question.question.lab==5){
+                        li.textContent = JSON.stringify(question.question.text) + " Date and Time Submitted: " + JSON.stringify(question.question.date)
+                        document.getElementById("list5").appendChild(li)
+                    }
+                    if(question.question.lab==6){
+                        li.textContent = JSON.stringify(question.question.text) + " Date and Time Submitted: " + JSON.stringify(question.question.date)
+                        document.getElementById("list6").appendChild(li)
+                    }
                 }
             }
         })
